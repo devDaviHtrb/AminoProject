150765b416959f33fc66d1df7a60f063486f54d5
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-09-19 10:34:33 -0300

    Delete routes directory

--- routes/Home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-from routes.GetPage import pages
-home = Blueprint("home", __name__)
-
-@home.route("/")
-def Home():
-
-    return render_template("Home.html", routes=["communities", "chat", "Login"]) #pages = pages.keys()) n tem pq colocar, pois as comunidades mais famosas são predefinidas, mas usei a renderização dinamica no menu da home
